2fd5ecf28fc6fb4362e183249ac4ab06a2122cce
Alvin Wu <alvinwu246@gmail.com>
2023-11-19 00:39:34 -0700

    pulling new model

--- neuroscribe-webapp/src/components/TextSelection.js
+++ neuroscribe-webapp/src/components/TextSelection.js
@@ -7,6 +7,63 @@ function TextSelection({ mood }) {
   const [boxesLocked, setBoxesLocked] = useState(Array(7).fill(false));
   const [currentIndex, setCurrentIndex] = useState(0);
 
+  const fetchDirection = async () => {
+    const url = 'http://127.0.0.1:5000/get-direction';
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Error fetching direction:", error);
+      throw error;
+    }
+  };
+
+  const simulateKeyPress = async () => {
+    try {
+      // Fetch direction data using fetchDirection function
+      const directionData = await fetchDirection();
+
+      // Assuming directionData contains the direction as a number (0, 1, 2, or 3)
+      const direction = directionData.direction;
+
+      // Simulate keypress based on the direction
+      switch (direction) {
+        case 0: // Up arrow key (key code 38)
+          // Simulate an up arrow key press
+          handleKeyDown({ keyCode: 38 });
+          break;
+        case 1: // Right arrow key (key code 39)
+          // Simulate a right arrow key press
+          handleKeyDown({ keyCode: 39 });
+          break;
+        case 2: // Down arrow key (key code 40)
+          // Simulate a down arrow key press
+          handleKeyDown({ keyCode: 40 });
+          break;
+        case 3: // Left arrow key (key code 37)
+          // Simulate a left arrow key press
+          handleKeyDown({ keyCode: 37 });
+          break;
+        default:
+          console.error('Invalid direction:', direction);
+      }
+    } catch (error) {
+      console.error('Error simulating keypress:', error);
+    }
+  };
+
+  useEffect(() => {
+    // Call simulateKeyPress to simulate the keypress based on fetched direction every 2 seconds
+    const intervalId = setInterval(simulateKeyPress, 3000);
+
+    // Cleanup function to clear the interval when the component unmounts
+    return () => clearInterval(intervalId);
+  }, []); // Empty dependency array means this effect runs once on component mount
+
   const fetchWords = async () => {
     let url;
     switch (mood) {
@@ -49,17 +106,21 @@ function TextSelection({ mood }) {
     console.log('Handling key down:', e.keyCode);
 
     if (e.keyCode === 38) { // Up arrow key
-      setCurrentSelections((current) =>
-        current.map((sel, index) =>
-          index === currentIndex ? HappyList[0] : sel
-        )
-      );
+      if (!boxesLocked[currentIndex]) {
+        setCurrentSelections((current) =>
+          current.map((sel, index) =>
+            index === currentIndex ? HappyList[0] : sel
+          )
+        );
+      }
     } else if (e.keyCode === 40) { // Down arrow key
-      setCurrentSelections((current) =>
-        current.map((sel, index) =>
-          index === currentIndex ? HappyList[1] : sel
-        )
-      );
+      if (!boxesLocked[currentIndex]) {
+        setCurrentSelections((current) =>
+          current.map((sel, index) =>
+            index === currentIndex ? HappyList[1] : sel
+          )
+        );
+      }
     } else if (e.keyCode === 39) { // Right arrow key to lock the box and move to the next
       setBoxesLocked((current) =>
         current.map((locked, index) =>
@@ -102,11 +163,15 @@ function TextSelection({ mood }) {
           <div key={index} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
             {index <= currentIndex && (
               <>
-                <div style={selectionBoxStyle} className='selection-box-above'>{index === currentIndex ? HappyList[0] : null}</div>
+                <div style={selectionBoxStyle} className='selection-box-above'>
+                  {!boxesLocked[currentIndex] && index === currentIndex ? HappyList[0] : null}
+                </div>
                 <div className="selection-box">
                   <u className='underline'>{word}</u>
                 </div>
-                <div style={selectionBoxStyle} className='selection-box-below'>{index === currentIndex ? HappyList[1] : null}</div>
+                <div style={selectionBoxStyle} className='selection-box-below'>
+                  {!boxesLocked[currentIndex] && index === currentIndex ? HappyList[1] : null}
+                </div>
               </>
             )}
           </div>
